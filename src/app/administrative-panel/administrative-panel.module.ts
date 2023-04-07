1530798fa126a645b69113f32c71dbb4fcf01a2d
Seba <sebastianrh.sarh@gmail.com>
2023-04-06 23:16:30 -0400

    add table of students

--- src/app/administrative-panel/administrative-panel.module.ts
+++ src/app/administrative-panel/administrative-panel.module.ts
@@ -5,22 +5,27 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { StudentsComponent } from '../pages/students/students.component';
+import { MatTableModule } from '@angular/material/table';
 
 
 
 @NgModule({
   declarations: [
-    AdministrativePanelComponent
+    AdministrativePanelComponent,
+    StudentsComponent
   ],
   imports: [
     CommonModule,
     MatSidenavModule,
     MatToolbarModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatTableModule
   ],
   exports: [
-    AdministrativePanelComponent
+    AdministrativePanelComponent,
+    StudentsComponent
   ]
 })
 export class AdministrativePanelModule { }

be67ce79190cfee40639fca0c7563c1dcd30cc36
Seba <sebastianrh.sarh@gmail.com>
2023-04-08 12:25:06 -0400

    add constant and interface

---
+++ src/app/constants/constant.ts
@@ -0,0 +1,14 @@
+import { PeriodicElement } from "../interface/students";
+
+export const ELEMENT_DATA: PeriodicElement[] = [
+  {name: 'Juan', lastName: "Menendez",  status: true, action: 'Button'},
+  {name: 'María', lastName: "Bonilla", status: true, action: 'Button'},
+  {name: 'José', lastName: "Palacios", status: false, action: 'Button'},
+  {name: 'Esmeralda', lastName: "Bustamante", status: true, action: 'Button'},
+  {name: 'Clemente', lastName: "Alamo", status: true, action: 'Button'},
+  {name: 'Yaiza', lastName: "Arce", status: false, action: 'Button'},
+  {name: 'Segundo', lastName: "Zamora", status: true, action: 'Button'},
+  {name: 'Diego', lastName: "Gamero", status: true, action: 'Button'},
+  {name: 'Celestina', lastName: "Nieves", status: true, action: 'Button'},
+  {name: 'Ofelia', lastName: "Hurtado", status: false, action: 'Button'},
+];
---
+++ src/app/interface/students.ts
@@ -0,0 +1,6 @@
+export interface PeriodicElement {
+  name: string;
+  lastName: string;
+  status: boolean;
+  action: string;
+}

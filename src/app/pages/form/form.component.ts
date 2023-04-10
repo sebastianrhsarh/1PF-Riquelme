ee6bdb01dc615961c09c27cf9d6c7e92a8c6da6f
Seba <sebastianrh.sarh@gmail.com>
2023-04-09 20:49:28 -0400

    redirect after form

--- src/app/pages/form/form.component.ts
+++ src/app/pages/form/form.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ELEMENT_DATA } from 'src/app/constants/constant';
 
 @Component({
@@ -13,7 +14,7 @@ export class FormComponent {
 
   registerForm: FormGroup;
 
-  constructor(public formBuilder: FormBuilder) {
+  constructor(public formBuilder: FormBuilder, private router:Router) {
 
     this.registerForm = this.formBuilder.group({
       name: this.nameControl,
@@ -31,7 +32,8 @@ export class FormComponent {
         status: false
       }
       ELEMENT_DATA.push(newFormat);
-      alert('Estudiante registrado correctamente')
+      alert('Estudiante registrado correctamente');
+      this.router.navigate(['/students']);
     } else {
       alert('El formulario no es valido');
     }
